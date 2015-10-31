2c492d6cec1a2abf5c2a7f3c122458fc1b6ae575
Srijayanth Sridhar <srijayanth@gmail.com>
2015-10-31 17:14:21 +0530

    Removed the calculation of amount into its own method and added some tests

--- src/com/tw/Customer.java
+++ src/com/tw/Customer.java
@@ -24,34 +24,20 @@ public class Customer {
         int frequentRenterPoints = 0;
         String result = "Rental Record for " + getName() + "\n";
 
-        for (Rental each: _rentals) {
-            double thisAmount = 0;
+        double thisAmount = 0;
+        for (Rental rental: _rentals) {
 
             //determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
+            thisAmount += amountPerRental(rental);
 
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+            if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1)
                 frequentRenterPoints++;
 
             // show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
+            result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
         }
 
@@ -60,5 +46,25 @@ public class Customer {
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 
         return result;
+    }
+
+    public double amountPerRental(Rental each) {
+        double thisAmount=0;
+        switch (each.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (each.getDaysRented() > 2)
+                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += each.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (each.getDaysRented() > 3)
+                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
     }
 }
---
+++ tests/com/tw/CustomerTest.java
@@ -0,0 +1,26 @@
+package com.tw;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CustomerTest {
+    @Test
+    public void testAmountPerRegularMovieRental() {
+        Movie topGun = new Movie("Top Gun", Movie.REGULAR);
+        Rental rental = new Rental(topGun, 1);
+        Customer john = new Customer("John");
+
+        assertEquals(2,john.amountPerRental(rental),0.0);
+    }
+
+    @Test
+    public void testAmountPerRegularMovieRentalWithLateDues() {
+        Movie topGun = new Movie("Top Gun", Movie.REGULAR);
+        Rental rental = new Rental(topGun, 3);
+        Customer john = new Customer("John");
+
+        assertEquals(3.5,john.amountPerRental(rental),0.0);
+    }
+
+}
